b0a46f0c2399ea46c8b7ff7d3d0eca4efc096225
Thomas LEMAITRE <thomas@squirrel.fr>
2019-10-08 14:32:19 +0400

    add rewarded video

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -230,3 +230,55 @@ if (OS_IOS) {
 
   $.win.add(btn);
 }
+
+var admobrewarded = require("net.birdirbir.admobrewarded");
+
+var instance = admobrewarded.createAd({
+  adUnitID: OS_IOS
+    ? "ca-app-pub-3940256099942544/1712485313"
+    : "ca-app-pub-3940256099942544/5224354917"
+});
+
+// Add listeners. Note that you need to show the ad after the
+// AD_REWARDED_VIDEO_AD_LOADED event.
+instance.addEventListener(admobrewarded.AD_REWARDED, function(e) {
+  Ti.API.warn("reward amount: ", e.rewardAmount);
+  Ti.API.warn("reward type: ", e.rewardType);
+});
+instance.addEventListener(
+  admobrewarded.AD_REWARDED_VIDEO_LEFT_APPLICATION,
+  function() {
+    Ti.API.warn("left application");
+  }
+);
+instance.addEventListener(
+  admobrewarded.AD_REWARDED_VIDEO_AD_CLOSED,
+  function() {
+    Ti.API.warn("ad closed");
+  }
+);
+instance.addEventListener(
+  admobrewarded.AD_REWARDED_VIDEO_FAILED_TO_LOAD,
+  function(e) {
+    Ti.API.warn("ad failed to load: ", e.errorCode);
+  }
+);
+instance.addEventListener(
+  admobrewarded.AD_REWARDED_VIDEO_AD_LOADED,
+  function() {
+    Ti.API.warn("video ad loaded");
+    instance.show();
+  }
+);
+instance.addEventListener(
+  admobrewarded.AD_REWARDED_VIDEO_AD_OPENED,
+  function() {
+    Ti.API.warn("video ad opened");
+  }
+);
+instance.addEventListener(admobrewarded.AD_REWARDED_VIDEO_STARTED, function() {
+  Ti.API.warn("video ad started");
+});
+
+// Load the ad.
+instance.loadAd();
